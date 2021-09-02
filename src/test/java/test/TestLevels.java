0cfa6ab0049299cdb1417f83ac89c539fffc29df
JoanFebrer <joan.febrer-nafria-external@capgemini.com>
2021-09-02 09:22:25 +0200

    Afegir path a chrome.driver

--- src/test/java/test/TestLevels.java
+++ src/test/java/test/TestLevels.java
@@ -36,6 +36,7 @@ public class TestLevels {
 	// INITIALIZE DRIVER
 	@BeforeClass
 	public static void initialize() {
+		System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
 		ArrayList<String> optionsList = new ArrayList<String>();
 		ChromeOptions chromeOptions = new ChromeOptions();
 		optionsList.add("--ignore-ssl-errors=yes");
